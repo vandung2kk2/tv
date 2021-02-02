6730661e5d884c2002e1065753a5012552bcf2f6
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2021-02-02 14:39:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+window.onload = function(){
+	var inp = document.getElementById("inputer"),
+		ids = document.getElementsByClassName("id");
+	var submit = document.getElementById("submit"),
+		focus = document.getElementById("ids");
+	focus.onclick = function(){
+		inp.focus();
+	}
+	inp.value = "";
+	last = 0;
+	inp.addEventListener("input", function(){
+		let idx=inp.value.length-1;
+		if(idx < last){
+			ids[last].innerText = "";
+		};
+		if(idx < 8 && idx >= 0){
+			ids[idx].innerText = inp.value[idx];
+		}
+		last = idx;
+	})
+	inp.onkeydown = function(k){
+		if(k.key == "Enter") submit.click()
+	}
+	submit.addEventListener("click",check);
+	let clicked = false;
+	var url="https://script.google.com/macros/s/AKfycbyYiS6Bne444MQ3-Q9n4K2eM8nQCsi-OdSm0xMQ5XI5OvcH_2eBuXIVVg/exec?id=";
+	function check(){
+		if(!clicked){
+			clicked = true;
+			focus.classList.remove("wrong");
+			submit.classList.add("clicked");
+			submit.innerText = "Processing...";
+			let resquest = new XMLHttpRequest();
+			resquest.open("GET",url + inp.value.slice(0,8));
+			resquest.onreadystatechange = function(){
+				if(this.readyState == 4){
+					console.log(this.responseText);
+					if(this.responseText == "false") focus.classList.add("wrong");
+					reset();
+					submit.classList.remove("clicked");
+					submit.innerText = "Confirm";
+					clicked = false;
+				}
+			}
+			resquest.send()
+		}
+	}
+	function reset(){
+		for(let i=0; i<8; i++){
+			ids[i].innerText = "";
+			inp.value="";
+			last = 0;
+		}
+	}
+}
